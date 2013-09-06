8a63ac4e89a38960a371b97a77c57215b374ef14
Emily <Lemmily0@gmail.com>
2013-09-07 00:05:02 +0100

    -discovered dungeon corridor making was lost. -needs re-doing.

--- src/sentient.py
+++ src/sentient.py
@@ -3,10 +3,6 @@ Created on 6 Sep 2013
 
 @author: Emily
 '''
-import json
-import json_sentient
-
-skill_list = json_sentient.skills
 
 skill_list_1 = [ #// 0_name:string, 1_attribute, 2_needTraining:Boolean, 3_desc:String,[4_dependsOn],[5_dependants]
                  ["Appraise", "int", False, "Used to analyse an item for monetary value, and contributing factors",["none"],["none"]],
@@ -21,12 +17,12 @@ class Skills:
         for line in skill_list_1:
             self.dict[line[0]] = Skill( line[0], line[1])
         
-        for key in skill_list:
-            print key.capitalize()
-            for attr in skill_list[key]: 
-                print attr, ": ", skill_list[key][attr]
         
-
+    def skill_level_check(self,skill):
+        
+        sk_exp = self.dict[skill].exp
+        
+        return sk_exp #TODO: nake lookup table to look up what level the skill is at. for now just retrun this/
 
 class Skill:
     def __init__(self,name,group="None"):
